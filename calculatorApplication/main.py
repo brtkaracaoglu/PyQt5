20564ee147b7911b47a9a6e7039e869253625dc5
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2023-07-30 11:54:59 +0300

    create other buttons

--- calculatorApplication/main.py
+++ calculatorApplication/main.py
@@ -21,8 +21,8 @@ class Calculator(QMainWindow):
                                      "border-radius: 5px; background-color: #e6e6fa;")
         self.entry_box.setText("0")
         self.entry_box.move(20,30)
-        ##########Number Buttons##########
 
+        ##########Number Buttons##########
         btn_number = []
         for i in range(1,10):
             i = QPushButton(str(i), self)
@@ -36,6 +36,7 @@ class Calculator(QMainWindow):
             for j in range(0,3):
                 btn_number[btn_index].move(25+j*90, 70+i*70)
                 btn_index += 1
+
         ##########Operator Buttons##########
         btn_operator = []
         for i in range(4):
@@ -51,6 +52,37 @@ class Calculator(QMainWindow):
         for i in range(4):
             btn_operator[i].move(290, 70+i*70)
 
+        ##########Other Buttons##########
+        btn_zero = QPushButton("0", self)
+        btn_zero.setStyleSheet("background-color: white")
+        btn_zero.setFont(QFont("Arial", 20))
+        btn_zero.resize(250, 40)
+        btn_zero.move(25,280)
+
+        btn_clear = QPushButton("C", self)
+        btn_clear.setStyleSheet("background-color: white")
+        btn_clear.setFont(QFont("Arial", 20))
+        btn_clear.resize(70, 40)
+        btn_clear.move(25, 340)
+
+        btn_dot = QPushButton(".", self)
+        btn_dot.setStyleSheet("background-color: white")
+        btn_dot.setFont(QFont("Arial", 15))
+        btn_dot.resize(70, 40)
+        btn_dot.move(110, 340)
+
+        btn_equal = QPushButton("=", self)
+        btn_equal.setStyleSheet("background-color: white")
+        btn_equal.setFont(QFont("Arial", 15))
+        btn_equal.resize(70, 40)
+        btn_equal.move(200, 340)
+
+        btn_delete = QPushButton(self)
+        btn_delete.setIcon(QIcon('icons/arrow.png'))
+        btn_delete.setStyleSheet("background-color: white")
+        btn_delete.setFont(QFont("Arial", 15))
+        btn_delete.resize(70, 40)
+        btn_delete.move(290, 340)
 
 
 def main():
