3407c842f648f158d8e3a57f1c9bd8667c64aa43
Berat Karacaoğlu <brtkaracaoglu@gmail.com>
2023-07-30 11:10:48 +0300

    create number buttons

--- calculatorApplication/main.py
+++ calculatorApplication/main.py
@@ -13,7 +13,33 @@ class Calculator(QMainWindow):
         self.show()
 
     def UI(self):
-        pass
+        ##########Entry Field##########
+
+        self.entry_box = QLineEdit(self)
+        self.entry_box.resize(335,30)
+        self.entry_box.setAlignment(Qt.AlignRight) # LineEdit yazısını sağ taraftan başlattık
+        self.entry_box.setStyleSheet("font: 14pt Arial Bold; border: 3px solid gray;"
+                                     "border-radius: 5px; background-color: #e6e6fa;")
+        self.entry_box.setText("0")
+        self.entry_box.move(20,30)
+
+        ##########Number Buttons##########
+        btn_number = []
+        for i in range(1,10):
+            i = QPushButton(str(i), self)
+            i.setFont(QFont("Arial", 15))
+            i.resize(70,40)
+            i.setStyleSheet("background-color: white")
+            btn_number.append(i)
+
+        btn_index = 0
+        for i in range(0,3):
+            for j in range(0,3):
+                btn_number[btn_index].move(25+j*90, 70+i*90)
+                btn_index += 1
+
+
+
 
 
 def main():
